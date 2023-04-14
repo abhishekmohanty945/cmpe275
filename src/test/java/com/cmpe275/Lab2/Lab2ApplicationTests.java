dc925329ee1d35a3ed5375e3aed61bc4f8a03ede
SaurabhWarathe <saurabhwarathe@gmail.com>
2023-04-14 13:54:29 -0700

    Lab2 starter files

---
+++ src/test/java/com/cmpe275/Lab2/Lab2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmpe275.Lab2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
